ef0a37c1cc08e7ef0e9fd9449cdf29929d56a4e8
unknown <felixlopezarm1@gmail.com>
2019-08-02 18:52:11 -0400

    added a simple navbar non- responsive

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
+      <Navbar />
       <Route path={"/"} exact component={Home} />
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavbarStyled = styled.nav`
+  background: var(--clr-primary);
+  width: 100%;
+  height: 60px;
+  color: var(--clr-black);
+  line-height: 60px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const NavbarContainer = styled.div`
+  max-width: 1248px;
+  margin: 0 auto;
+  padding: 0 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LogoContainer = styled.div`
+  font-size: 3.2rem;
+  font-weight: 700;
+  cursor: pointer;
+`;
+
+const NavigationLinks = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+const Link = styled.li`
+  font-size: 1.8rem;
+  transition: all 600ms cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  a {
+    text-decoration: none;
+    color: var(--clr-black);
+    margin: 0 1rem;
+  }
+
+  &:hover {
+    transform: translateY(-3px);
+  }
+
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <NavbarStyled>
+      <NavbarContainer>
+        <LogoContainer>Computers</LogoContainer>
+        <NavigationLinks>
+          <Link>
+            <a href="#">About</a>
+          </Link>
+          <Link>
+            <a href="#">Stores</a>
+          </Link>
+          <Link>
+            <a href="#">Contact</a>
+          </Link>
+        </NavigationLinks>
+      </NavbarContainer>
+    </NavbarStyled>
+  );
+};
+
+export default Navbar;
