b01d1e8c56af57c37cd97a38be1b4a40e05f3810
unknown <felixlopezarm1@gmail.com>
2019-08-01 20:50:25 -0400

    Refactor hero elements to new react component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,11 @@
 import React from "react";
 
-// import HeroSvg from "./svg/Hero.svg";
-import LaptopGirl from "../svg/laptop-girl.svg";
+import Hero from "./Hero";
 
 const Home = () => {
   return (
     <div>
-      <div className="hero">
-        <div className="hero-img-container">
-          <div className="call-to-action">
-            <div className="laptop-girl-container">
-              <img src={LaptopGirl} alt="laptop girl" />
-            </div>
-            <div className="text-container">
-              <h1 className="big-text">find your perfect computer with us.</h1>
-              <button className="btn">go shop now</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Hero />
       <main className="main">
         <h1 className="our-products">Our Products</h1>
       </main>
