cf49d8415120a6f547cd064ec7944857a88b9e2a
unknown <felixlopezarm1@gmail.com>
2019-08-03 21:32:08 -0400

    change font weight and border values

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,11 +8,11 @@ const StyledButton = styled.button`
   padding: 0.2em 1.1em;
   text-transform: uppercase;
   font-size: 3.1rem;
-  font-weight: 800;
+  font-weight: 700;
   display: flex;
   align-items: center;
   border: none;
-  border-radius: 15px;
+  border-radius: 4px;
   letter-spacing: -1px;
   transition: all 600ms cubic-bezier(0.075, 0.82, 0.165, 1);
 
