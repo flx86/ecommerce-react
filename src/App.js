565369139bee59600e177720fb650aabf3307912
unknown <felixlopezarm1@gmail.com>
2019-08-03 21:31:22 -0400

    Added font awesome icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { Route } from "react-router-dom";
-
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHeart,
+  faCartPlus,
+  faChevronUp,
+  faChevronDown,
+  faEye,
+  faShoppingCart
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faHeart,
+  faCartPlus,
+  faChevronUp,
+  faChevronDown,
+  faEye,
+  faShoppingCart
+);
+
 function App() {
   return (
     <div>
